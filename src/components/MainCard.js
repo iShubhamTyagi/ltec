d3724855f480968d01c6d46ea4b27d802a163db2
Shubham Tyagi <ishubhamtyagi2021@gmail.com>
2023-06-25 12:30:07 +0530

    Improved mobile layout

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { Card, CardContent, Typography, Radio, RadioGroup, FormControlLabel, Button, Grid, CircularProgress, LinearProgress } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import QuestionCard1a from './QuestionCard1a';
 import QuestionCard1b from './QuestionCard1b';
 import QuestionCard2a from './QuestionCard2a';
@@ -9,17 +7,15 @@ import QuestionCard3a from './QuestionCard3a';
 import QuestionCard3b from './QuestionCard3b';
 import QuestionCardc1 from './QuestionCardc1';
 import QuestionCardc2 from './QuestionCardc2';
-
-const questionCardSequences = [
-  [<QuestionCard1a />, <QuestionCard1b />, <QuestionCardc1 />, <QuestionCardc2 />],
-  [<QuestionCard2a />, <QuestionCard2b />, <QuestionCardc1 />, <QuestionCardc2 />],
-  [<QuestionCard3a />, <QuestionCard3b />, <QuestionCardc1 />, <QuestionCardc2 />],
-];
+import { Card, CardContent, Typography, Radio, RadioGroup, FormControlLabel, Button, Grid, CircularProgress, LinearProgress } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 const MainCardContainer = styled(Card)(({ theme }) => ({
   margin: theme.spacing(2),
   boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
   position: 'relative',
+  overflow: 'hidden',
+  height: '100vh', // Set container height to fill the available vertical space
 }));
 
 const ProgressContainer = styled('div')(({ theme }) => ({
@@ -33,6 +29,10 @@ const MainCardContent = styled(CardContent)(({ theme }) => ({
   flexDirection: 'column',
   alignItems: 'center',
   padding: theme.spacing(3),
+  position: 'relative',
+  overflow: 'auto',
+  maxHeight: 'calc(100% - 70px)', // Adjust the max height based on your layout
+  height: '100%', // Utilize the available vertical space
 
   [theme.breakpoints.up('sm')]: {
     padding: theme.spacing(6),
@@ -62,17 +62,43 @@ const ProgressContainerOuter = styled(Grid)({
   width: '100%',
 });
 
+const UserSelection = styled(Typography)(({ theme }) => ({
+  position: 'sticky',
+  top: 0,
+  background: theme.palette.background.paper,
+  padding: theme.spacing(2),
+  zIndex: 1,
+}));
+
+const questionCardSequences = [
+  {
+    label: 'COPD',
+    cards: [<QuestionCard1a />, <QuestionCard1b />, <QuestionCardc1 />, <QuestionCardc2 />],
+  },
+  {
+    label: 'ILD',
+    cards: [<QuestionCard2a />, <QuestionCard2b />, <QuestionCardc1 />, <QuestionCardc2 />],
+  },
+  {
+    label: 'Bronchiectasis',
+    cards: [<QuestionCard3a />, <QuestionCard3b />, <QuestionCardc1 />, <QuestionCardc2 />],
+  },
+];
+
 function MainCard() {
   const [selectedSequence, setSelectedSequence] = useState(null);
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [userSelection, setUserSelection] = useState('');
 
   const handleSelection = (event) => {
-    setSelectedSequence(Number(event.target.value));
+    const sequence = Number(event.target.value);
+    setSelectedSequence(sequence);
     setCurrentCardIndex(1);
+    setUserSelection(questionCardSequences[sequence].label);
   };
 
   const handleNext = () => {
-    if (selectedSequence !== null && currentCardIndex < questionCardSequences[selectedSequence].length) {
+    if (selectedSequence !== null && currentCardIndex < questionCardSequences[selectedSequence].cards.length) {
       setCurrentCardIndex(currentCardIndex + 1);
     }
   };
@@ -83,10 +109,11 @@ function MainCard() {
     } else if (currentCardIndex === 1) {
       setSelectedSequence(null);
       setCurrentCardIndex(0);
+      setUserSelection('');
     }
   };
 
-  const progress = (currentCardIndex / (questionCardSequences[selectedSequence]?.length || 1)) * 100;
+  const progress = (currentCardIndex / (questionCardSequences[selectedSequence]?.cards.length || 1)) * 100;
 
   return (
     <MainCardContainer>
@@ -94,20 +121,29 @@ function MainCard() {
         <CircularProgress variant="determinate" value={progress} />
       </ProgressContainer>
       <MainCardContent>
+        {userSelection && (
+          <UserSelection variant="body1" component="div">
+            {userSelection}
+          </UserSelection>
+        )}
         {selectedSequence === null ? (
           <>
             <MainCardTitle variant="h6" component="div">
-              Select a question card sequence:
+              Choose the Patient Disease:
             </MainCardTitle>
             <RadioGroupContainer row onChange={handleSelection}>
-              <FormControlLabel value="0" control={<Radio />} label="Sequence 1" />
-              <FormControlLabel value="1" control={<Radio />} label="Sequence 2" />
-              <FormControlLabel value="2" control={<Radio />} label="Sequence 3" />
+              {questionCardSequences.map((sequence, index) => (
+                <FormControlLabel key={index} value={index.toString()} control={<Radio />} label={sequence.label} />
+              ))}
             </RadioGroupContainer>
           </>
         ) : (
           <>
-            {questionCardSequences[selectedSequence][currentCardIndex - 1]}
+            {questionCardSequences[selectedSequence].cards.map((card, index) => (
+              <div key={index} style={{ display: index === currentCardIndex - 1 ? 'block' : 'none' }}>
+                {card}
+              </div>
+            ))}
             <ButtonsContainer container spacing={2}>
               <Grid item xs={6} sm={3}>
                 <Button variant="contained" color="primary" onClick={handlePrevious}>
--- src/components/QuestionCard1a.js
+++ src/components/QuestionCard1a.js
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Grid from '@mui/material/Grid';
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Typography, Radio, RadioGroup, FormControlLabel, Grid, Box } from '@mui/material';
 
 const questions = [
   "Card 1a",
   "Question 2?",
   "Question 3?",
   "Question 4?",
-  "Question 5?",
+  "Clinical Deterioration on maximal therapy (including oxygen, NIV, rehabilitation)",
 ];
 
 function QuestionCard1a() {
   const [answers, setAnswers] = useState(Array(questions.length).fill(null));
+  const [isMobile, setIsMobile] = useState(false);
 
   const handleAnswer = (index, answer) => {
     const newAnswers = [...answers];
@@ -24,25 +19,52 @@ function QuestionCard1a() {
     setAnswers(newAnswers);
   };
 
+  useEffect(() => {
+    const updateViewport = () => {
+      setIsMobile(window.innerWidth < 600); // Adjust the breakpoint as needed
+    };
+
+    window.addEventListener('resize', updateViewport);
+    updateViewport();
+
+    return () => window.removeEventListener('resize', updateViewport);
+  }, []);
+
   return (
-    <Card>
-      <CardContent>
-        {questions.map((question, index) => (
-          <Grid container alignItems="center" spacing={2} key={index}>
-            <Grid item xs={8}>
-              <Typography variant="h6" component="div">
-                {question}
-              </Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <RadioGroup row value={answers[index]} onChange={(event) => handleAnswer(index, event.target.value)}>
-                <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-                <FormControlLabel value="No" control={<Radio />} label="No" />
-              </RadioGroup>
-            </Grid>
-          </Grid>
-        ))}
+    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <CardContent sx={{ flex: '1 1 auto', overflow: 'auto', padding: 0 }}>
+        <Box sx={{ padding: 2 }}>
+          {questions.map((question, index) => (
+            <Box key={index} sx={{ marginBottom: 2 }}>
+              <Grid container alignItems="center" spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <Typography
+                    variant="body1"
+                    component="div"
+                    sx={{
+                      width: '100%',
+                      paddingRight: isMobile ? 'inherit' : '32px', // Add extra right padding for mobile view
+                    }}
+                  >
+                    {question}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <RadioGroup
+                    row
+                    value={answers[index]}
+                    onChange={(event) => handleAnswer(index, event.target.value)}
+                  >
+                    <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                    <FormControlLabel value="No" control={<Radio />} label="No" />
+                  </RadioGroup>
+                </Grid>
+              </Grid>
+            </Box>
+          ))}
+        </Box>
       </CardContent>
+      <Box sx={{ flexGrow: 1 }}></Box> {/* Fill the remaining space */}
     </Card>
   );
 }
