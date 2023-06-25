cd865aa0b86d13d45c4b3eba044e31aa4d33e2e8
Shubham Tyagi <ishubhamtyagi2021@gmail.com>
2023-06-25 11:40:43 +0530

    Added progress bars indicator for each workflow

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Button from '@mui/material/Button';
-import LinearProgress from '@mui/material/LinearProgress';
+import { Card, CardContent, Typography, Radio, RadioGroup, FormControlLabel, Button, Grid, CircularProgress, LinearProgress } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import QuestionCard1a from './QuestionCard1a';
 import QuestionCard1b from './QuestionCard1b';
 import QuestionCard2a from './QuestionCard2a';
@@ -21,6 +15,52 @@ const questionCardSequences = [
   [<QuestionCard2a />, <QuestionCard2b />, <QuestionCardc1 />, <QuestionCardc2 />],
   [<QuestionCard3a />, <QuestionCard3b />, <QuestionCardc1 />, <QuestionCardc2 />],
 ];
+
+const MainCardContainer = styled(Card)(({ theme }) => ({
+  margin: theme.spacing(2),
+  boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+  position: 'relative',
+}));
+
+const ProgressContainer = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  top: theme.spacing(1),
+  right: theme.spacing(1),
+}));
+
+const MainCardContent = styled(CardContent)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: theme.spacing(3),
+
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(6),
+  },
+}));
+
+const MainCardTitle = styled(Typography)(({ theme }) => ({
+  fontSize: theme.typography.pxToRem(20),
+  marginBottom: theme.spacing(2),
+
+  [theme.breakpoints.up('sm')]: {
+    fontSize: theme.typography.pxToRem(24),
+  },
+}));
+
+const RadioGroupContainer = styled(RadioGroup)({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const ButtonsContainer = styled(Grid)({
+  marginTop: '24px',
+});
+
+const ProgressContainerOuter = styled(Grid)({
+  marginTop: '12px',
+  width: '100%',
+});
 
 function MainCard() {
   const [selectedSequence, setSelectedSequence] = useState(null);
@@ -49,33 +89,44 @@ function MainCard() {
   const progress = (currentCardIndex / (questionCardSequences[selectedSequence]?.length || 1)) * 100;
 
   return (
-    <Card>
-      <CardContent>
+    <MainCardContainer>
+      <ProgressContainer>
+        <CircularProgress variant="determinate" value={progress} />
+      </ProgressContainer>
+      <MainCardContent>
         {selectedSequence === null ? (
           <>
-            <Typography variant="h6" component="div">
+            <MainCardTitle variant="h6" component="div">
               Select a question card sequence:
-            </Typography>
-            <RadioGroup row onChange={handleSelection}>
+            </MainCardTitle>
+            <RadioGroupContainer row onChange={handleSelection}>
               <FormControlLabel value="0" control={<Radio />} label="Sequence 1" />
               <FormControlLabel value="1" control={<Radio />} label="Sequence 2" />
               <FormControlLabel value="2" control={<Radio />} label="Sequence 3" />
-            </RadioGroup>
+            </RadioGroupContainer>
           </>
         ) : (
           <>
             {questionCardSequences[selectedSequence][currentCardIndex - 1]}
-            <Button variant="contained" color="primary" onClick={handlePrevious}>
-              Previous
-            </Button>
-            <Button variant="contained" color="primary" onClick={handleNext}>
-              Next
-            </Button>
+            <ButtonsContainer container spacing={2}>
+              <Grid item xs={6} sm={3}>
+                <Button variant="contained" color="primary" onClick={handlePrevious}>
+                  Previous
+                </Button>
+              </Grid>
+              <Grid item xs={6} sm={3}>
+                <Button variant="contained" color="primary" onClick={handleNext}>
+                  Next
+                </Button>
+              </Grid>
+            </ButtonsContainer>
           </>
         )}
+      </MainCardContent>
+      <ProgressContainerOuter>
         <LinearProgress variant="determinate" value={progress} />
-      </CardContent>
-    </Card>
+      </ProgressContainerOuter>
+    </MainCardContainer>
   );
 }
 
