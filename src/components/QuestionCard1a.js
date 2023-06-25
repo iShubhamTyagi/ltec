59a16aa518bbf85c500182816f2eae0479671c0c
Shubham Tyagi <ishubhamtyagi2021@gmail.com>
2023-06-25 13:33:34 +0530

    improved card layouts

--- src/components/QuestionCard1a.js
+++ src/components/QuestionCard1a.js
@@ -1,22 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import { Card, CardContent, Typography, Radio, RadioGroup, FormControlLabel, Grid, Box } from '@mui/material';
+import { styled } from '@mui/system';
 
 const questions = [
-  "Card 1a",
-  "Question 2?",
-  "Question 3?",
-  "Question 4?",
-  "Clinical Deterioration on maximal therapy (including oxygen, NIV, rehabilitation)",
+  {
+    text: "Card 1a",
+    subheading: null,
+  },
+  {
+    text: "Question 2?",
+    subheading: "Group 1",
+  },
+  {
+    text: "Question 3?",
+    subheading: "Group 1",
+  },
+  {
+    text: "Question 4?",
+    subheading: "Group 2",
+  },
+  {
+    text: "Clinical Deterioration on maximal therapy (including oxygen, NIV, rehabilitation)",
+    subheading: "Group 2",
+  },
 ];
+
+const MainCardContainer = styled(Card)(({ theme }) => ({
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
+const MainCardContent = styled(CardContent)({
+  flex: '1 1 auto',
+  overflow: 'auto',
+  padding: 0,
+});
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(2),
+}));
+
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(1),
+  fontWeight: 'bold',
+}));
+
+const QuestionContainer = styled(Grid)(({ theme }) => ({
+  alignItems: 'center',
+  paddingLeft: theme.spacing(2),
+}));
 
 function QuestionCard1a() {
   const [answers, setAnswers] = useState(Array(questions.length).fill(null));
   const [isMobile, setIsMobile] = useState(false);
 
-  const handleAnswer = (index, answer) => {
-    const newAnswers = [...answers];
-    newAnswers[index] = answer;
-    setAnswers(newAnswers);
+  const handleAnswer = (groupIndex, questionIndex, answer) => {
+    const updatedAnswers = [...answers];
+    const index = groupedQuestions[groupIndex].questions[questionIndex].index;
+    updatedAnswers[index] = answer;
+    setAnswers(updatedAnswers);
   };
 
   useEffect(() => {
@@ -30,42 +73,58 @@ function QuestionCard1a() {
     return () => window.removeEventListener('resize', updateViewport);
   }, []);
 
+  const groupedQuestions = questions.reduce((groups, question, index) => {
+    const prevQuestion = questions[index - 1];
+    const currentGroup = groups[groups.length - 1];
+
+    if (prevQuestion && prevQuestion.subheading === question.subheading) {
+      currentGroup.questions.push({ ...question, index });
+    } else {
+      groups.push({ subheading: question.subheading, questions: [{ ...question, index }] });
+    }
+
+    return groups;
+  }, []);
+
   return (
-    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      <CardContent sx={{ flex: '1 1 auto', overflow: 'auto', padding: 0 }}>
-        <Box sx={{ padding: 2 }}>
-          {questions.map((question, index) => (
-            <Box key={index} sx={{ marginBottom: 2 }}>
-              <Grid container alignItems="center" spacing={2}>
-                <Grid item xs={12} md={6}>
-                  <Typography
-                    variant="body1"
-                    component="div"
-                    sx={{
-                      width: '100%',
-                      paddingRight: isMobile ? 'inherit' : '32px', // Add extra right padding for mobile view
-                    }}
-                  >
-                    {question}
-                  </Typography>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <RadioGroup
-                    row
-                    value={answers[index]}
-                    onChange={(event) => handleAnswer(index, event.target.value)}
-                  >
-                    <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="No" control={<Radio />} label="No" />
-                  </RadioGroup>
-                </Grid>
-              </Grid>
+    <MainCardContainer>
+      <MainCardContent>
+        <StyledBox>
+          <Typography variant="h5" component="div" sx={{ marginBottom: 2, textAlign: 'left' }}>
+            General Heading
+          </Typography>
+          {groupedQuestions.map((group, groupIndex) => (
+            <Box key={groupIndex} sx={{ marginBottom: 2 }}>
+              {group.subheading && (
+                <StyledTypography variant="subtitle1" component="div" sx={{ textAlign: 'left' }}>
+                  {group.subheading}
+                </StyledTypography>
+              )}
+              {group.questions.map((question, questionIndex) => (
+                <QuestionContainer container key={questionIndex}>
+                  <Grid item xs={12} md={6}>
+                    <Typography variant="body1" component="div">
+                      {question.text}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                    <RadioGroup
+                      row
+                      value={answers[question.index]}
+                      onChange={(event) => handleAnswer(groupIndex, questionIndex, event.target.value)}
+                    >
+                      <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                      <FormControlLabel value="No" control={<Radio />} label="No" />
+                    </RadioGroup>
+                  </Grid>
+                </QuestionContainer>
+              ))}
             </Box>
           ))}
-        </Box>
-      </CardContent>
+        </StyledBox>
+      </MainCardContent>
       <Box sx={{ flexGrow: 1 }}></Box> {/* Fill the remaining space */}
-    </Card>
+    </MainCardContainer>
   );
 }
 
--- src/components/QuestionCard2b.js
+++ src/components/QuestionCard2b.js
@@ -1,49 +1,130 @@
-import React, { useState } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Grid from '@mui/material/Grid';
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Typography, Radio, RadioGroup, FormControlLabel, Grid, Box } from '@mui/material';
+import { styled } from '@mui/system';
 
 const questions = [
-  "Card 2b?",
-  "Question 2?",
-  "Question 3?",
-  "Question 4?",
-  "Question 5?",
+  {
+    text: "Card 2b",
+    subheading: null,
+  },
+  {
+    text: "Question 2?",
+    subheading: "Group 1",
+  },
+  {
+    text: "Question 3?",
+    subheading: "Group 1",
+  },
+  {
+    text: "Question 4?",
+    subheading: "Group 2",
+  },
+  {
+    text: "Clinical Deterioration on maximal therapy (including oxygen, NIV, rehabilitation)",
+    subheading: "Group 2",
+  },
 ];
+
+const MainCardContainer = styled(Card)(({ theme }) => ({
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
+const MainCardContent = styled(CardContent)({
+  flex: '1 1 auto',
+  overflow: 'auto',
+  padding: 0,
+});
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(2),
+}));
+
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(1),
+  fontWeight: 'bold',
+}));
+
+const QuestionContainer = styled(Grid)(({ theme }) => ({
+  alignItems: 'center',
+  paddingLeft: theme.spacing(2),
+}));
 
 function QuestionCard2b() {
   const [answers, setAnswers] = useState(Array(questions.length).fill(null));
+  const [isMobile, setIsMobile] = useState(false);
 
-  const handleAnswer = (index, answer) => {
-    const newAnswers = [...answers];
-    newAnswers[index] = answer;
-    setAnswers(newAnswers);
+  const handleAnswer = (groupIndex, questionIndex, answer) => {
+    const updatedAnswers = [...answers];
+    const index = groupedQuestions[groupIndex].questions[questionIndex].index;
+    updatedAnswers[index] = answer;
+    setAnswers(updatedAnswers);
   };
 
+  useEffect(() => {
+    const updateViewport = () => {
+      setIsMobile(window.innerWidth < 600); // Adjust the breakpoint as needed
+    };
+
+    window.addEventListener('resize', updateViewport);
+    updateViewport();
+
+    return () => window.removeEventListener('resize', updateViewport);
+  }, []);
+
+  const groupedQuestions = questions.reduce((groups, question, index) => {
+    const prevQuestion = questions[index - 1];
+    const currentGroup = groups[groups.length - 1];
+
+    if (prevQuestion && prevQuestion.subheading === question.subheading) {
+      currentGroup.questions.push({ ...question, index });
+    } else {
+      groups.push({ subheading: question.subheading, questions: [{ ...question, index }] });
+    }
+
+    return groups;
+  }, []);
+
   return (
-    <Card>
-      <CardContent>
-        {questions.map((question, index) => (
-          <Grid container alignItems="center" spacing={2} key={index}>
-            <Grid item xs={8}>
-              <Typography variant="h6" component="div">
-                {question}
-              </Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <RadioGroup row value={answers[index]} onChange={(event) => handleAnswer(index, event.target.value)}>
-                <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-                <FormControlLabel value="No" control={<Radio />} label="No" />
-              </RadioGroup>
-            </Grid>
-          </Grid>
-        ))}
-      </CardContent>
-    </Card>
+    <MainCardContainer>
+      <MainCardContent>
+        <StyledBox>
+          <Typography variant="h5" component="div" sx={{ marginBottom: 2, textAlign: 'left' }}>
+            General Heading
+          </Typography>
+          {groupedQuestions.map((group, groupIndex) => (
+            <Box key={groupIndex} sx={{ marginBottom: 2 }}>
+              {group.subheading && (
+                <StyledTypography variant="subtitle1" component="div" sx={{ textAlign: 'left' }}>
+                  {group.subheading}
+                </StyledTypography>
+              )}
+              {group.questions.map((question, questionIndex) => (
+                <QuestionContainer container key={questionIndex}>
+                  <Grid item xs={12} md={6}>
+                    <Typography variant="body1" component="div">
+                      {question.text}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                    <RadioGroup
+                      row
+                      value={answers[question.index]}
+                      onChange={(event) => handleAnswer(groupIndex, questionIndex, event.target.value)}
+                    >
+                      <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                      <FormControlLabel value="No" control={<Radio />} label="No" />
+                    </RadioGroup>
+                  </Grid>
+                </QuestionContainer>
+              ))}
+            </Box>
+          ))}
+        </StyledBox>
+      </MainCardContent>
+      <Box sx={{ flexGrow: 1 }}></Box> {/* Fill the remaining space */}
+    </MainCardContainer>
   );
 }
 
