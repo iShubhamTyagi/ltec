9eca9b139d5cdd4c79562a4c1fe4ce2e7bdb4f7d
Shubham Tyagi <ishubhamtyagi2021@gmail.com>
2023-06-25 18:15:38 +0530

    Handled crashes on choice selection

--- src/components/QuestionCard1b.js
+++ src/components/QuestionCard1b.js
@@ -62,11 +62,7 @@ const QuestionContainer = styled(Grid)(({ theme }) => ({
 
 function QuestionCard1b({ answers, setAnswers }) {
   const handleAnswer = (index, answer) => {
-    setAnswers((prevState) => {
-      const updatedAnswers = [...prevState];
-      updatedAnswers[index] = answer;
-      return updatedAnswers;
-    });
+    setAnswers(index, answer);
   };
 
   const groupedQuestions = questions.reduce((groups, question, index) => {
