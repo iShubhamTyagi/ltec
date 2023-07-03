776986064f6e37c469184e2bab9647b94fbee8b4
Shubham Tyagi <ishubhamtyagi2021@gmail.com>
2023-07-03 17:44:59 +0530

    Fixed state management issue

--- src/components/QuestionCard2b.js
+++ src/components/QuestionCard2b.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Card,
   CardContent,
@@ -8,45 +8,45 @@ import {
   FormControlLabel,
   Grid,
   Box,
-} from '@mui/material';
-import { styled } from '@mui/system';
+} from "@mui/material";
+import { styled } from "@mui/system";
 
 const questions = [
   {
-    text: 'FVC > 10%',
-    subheading: 'Pulmonary Fibrosis with past 6 months absolute decline',
+    text: "FVC > 10%",
+    subheading: "Pulmonary Fibrosis with past 6 months absolute decline",
   },
   {
-    text: 'DLCO > 10%',
-    subheading: 'Pulmonary Fibrosis with past 6 months absolute decline',
+    text: "DLCO > 10%",
+    subheading: "Pulmonary Fibrosis with past 6 months absolute decline",
   },
   {
-    text: 'FVC > 5% with radiological worsening',
-    subheading: 'Pulmonary Fibrosis with past 6 months absolute decline',
+    text: "FVC > 5% with radiological worsening",
+    subheading: "Pulmonary Fibrosis with past 6 months absolute decline",
   },
   {
-    text: '6 minute walk test SPO2 < 88% / 50 mtrs decline in 6 month',
+    text: "6 minute walk test SPO2 < 88% / 50 mtrs decline in 6 month",
     subheading: null,
   },
   {
-    text: 'Pulmonary hypertension',
+    text: "Pulmonary hypertension",
     subheading: null,
   },
   {
-    text: 'Hospitalisation due to exacerbation/pneumothorax/respiratory decline',
+    text: "Hospitalisation due to exacerbation/pneumothorax/respiratory decline",
     subheading: null,
   },
 ];
 
 const MainCardContainer = styled(Card)(({ theme }) => ({
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
 }));
 
 const MainCardContent = styled(CardContent)({
-  flex: '1 1 auto',
-  overflow: 'auto',
+  flex: "1 1 auto",
+  overflow: "auto",
   padding: 0,
 });
 
@@ -56,17 +56,20 @@ const StyledBox = styled(Box)(({ theme }) => ({
 
 const StyledTypography = styled(Typography)(({ theme }) => ({
   marginBottom: theme.spacing(1),
-  fontWeight: 'bold',
+  fontWeight: "bold",
 }));
 
 const QuestionContainer = styled(Grid)(({ theme }) => ({
-  alignItems: 'center',
+  alignItems: "center",
   paddingLeft: theme.spacing(2),
 }));
 
-function QuestionCard2b({ answers, setAnswers }) {
-  const handleAnswer = (index, answer) => {
-    setAnswers(index, answer);
+function QuestionCard2b({ answers, setAnswers, currentCardIndex }) {
+  const handleAnswer = (questionIndex, answer) => {
+    // Calculate a unique index for the question
+    const uniqueIndex =
+      (currentCardIndex - 1) * questions.length + questionIndex;
+    setAnswers(uniqueIndex, answer);
   };
 
   const groupedQuestions = questions.reduce((groups, question, index) => {
@@ -76,7 +79,10 @@ function QuestionCard2b({ answers, setAnswers }) {
     if (prevQuestion && prevQuestion.subheading === question.subheading) {
       currentGroup.questions.push({ ...question, index });
     } else {
-      groups.push({ subheading: question.subheading, questions: [{ ...question, index }] });
+      groups.push({
+        subheading: question.subheading,
+        questions: [{ ...question, index }],
+      });
     }
 
     return groups;
@@ -86,31 +92,58 @@ function QuestionCard2b({ answers, setAnswers }) {
     <MainCardContainer>
       <MainCardContent>
         <StyledBox>
-          <Typography variant="h5" component="div" sx={{ marginBottom: 2, textAlign: 'left' }}>
-          Is the Patient eligible for listing?
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{ marginBottom: 2, textAlign: "left" }}
+          >
+            Is the Patient eligible for listing?
           </Typography>
           {groupedQuestions.map((group, groupIndex) => (
             <Box key={groupIndex} sx={{ marginBottom: 2 }}>
               {group.subheading && (
-                <StyledTypography variant="subtitle1" component="div" sx={{ textAlign: 'left' }}>
+                <StyledTypography
+                  variant="subtitle1"
+                  component="div"
+                  sx={{ textAlign: "left" }}
+                >
                   {group.subheading}
                 </StyledTypography>
               )}
               {group.questions.map((question) => (
                 <QuestionContainer container key={question.index}>
                   <Grid item xs={12} md={6}>
-                    <Typography variant="body1" component="div" sx={{ textAlign: 'left' }}>
+                    <Typography
+                      variant="body1"
+                      component="div"
+                      sx={{ textAlign: "left" }}
+                    >
                       {question.text}
                     </Typography>
                   </Grid>
-                  <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                  <Grid
+                    item
+                    xs={12}
+                    md={6}
+                    sx={{ display: "flex", justifyContent: "flex-start" }}
+                  >
                     <RadioGroup
                       row
-                      value={answers[question.index]}
-                      onChange={(event) => handleAnswer(question.index, event.target.value)}
+                      value={answers[`${currentCardIndex}-${question.index}`]} // Use the unique key to get the answer
+                      onChange={(event) =>
+                        handleAnswer(question.index, event.target.value)
+                      }
                     >
-                      <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-                      <FormControlLabel value="No" control={<Radio />} label="No" />
+                      <FormControlLabel
+                        value="Yes"
+                        control={<Radio />}
+                        label="Yes"
+                      />
+                      <FormControlLabel
+                        value="No"
+                        control={<Radio />}
+                        label="No"
+                      />
                     </RadioGroup>
                   </Grid>
                 </QuestionContainer>
