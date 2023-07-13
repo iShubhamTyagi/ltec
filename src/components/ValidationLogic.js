466005ee93c05c3dc98657672e4f7e9339e6114a
Shubham Tyagi <ishubhamtyagi2021@gmail.com>
2023-07-13 18:47:05 +0530

    added validation logic

---
+++ src/components/ValidationLogic.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ValidationLogic({ questionCardIndex, answers }) {
+  let isEligible = false;
+  if (questionCardIndex <= 2) {
+    // First or second card
+    isEligible = Object.values(answers).some((answer) => answer === "Yes");
+  } else if (questionCardIndex > 2 && questionCardIndex <= 4) {
+    // Third or fourth card
+    isEligible = !Object.values(answers).some((answer) => answer === "Yes");
+  }
+
+  return isEligible ? "Eligible" : "Ineligible";
+}
+
+export default ValidationLogic;
