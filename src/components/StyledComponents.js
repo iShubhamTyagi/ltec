d143c054c17ae4f88868548226e945a14f52b8a8
Shubham Tyagi <ishubhamtyagi2021@gmail.com>
2023-07-15 19:55:02 +0530

    Final Card and footer changes

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -3,27 +3,27 @@ import { Card, CardContent, Typography, RadioGroup, Grid, Box } from "@mui/mater
 
 // Styled components for MainCard component
 export const MainCardContainer = styled(Card)(({ theme }) => ({
-    margin: "auto", // Center horizontally
-    marginTop: "1%", // Adjust the top margin as desired
-    marginBottom: "2%", // Adjust the bottom margin as desired
-    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-    position: "relative",
-    overflow: "hidden",
-    height: "calc(100vh - 10% - 16px)", // Subtract the height of the buttons container and the margin
-    width: "90%", // Adjust the width as desired
-  
-    [theme.breakpoints.up("sm")]: {
-      width: "80%", // Adjust the width for larger screens
-    },
-  
-    [theme.breakpoints.up("md")]: {
-      width: "70%", // Adjust the width for even larger screens
-    },
-  
-    [theme.breakpoints.up("lg")]: {
-      width: "60%", // Adjust the width for larger desktop screens
-    },
-  }));
+  margin: "auto", // Center horizontally
+  marginTop: "1%", // Adjust the top margin as desired
+  marginBottom: "2%", // Adjust the bottom margin as desired
+  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  position: "relative",
+  overflow: "hidden",
+  width: "100%", // Adjust the width to occupy the full width of the container
+
+  [theme.breakpoints.up("sm")]: {
+    width: "90%", // Adjust the width for larger screens
+  },
+
+  [theme.breakpoints.up("md")]: {
+    width: "80%", // Adjust the width for even larger screens
+  },
+
+  [theme.breakpoints.up("lg")]: {
+    width: "70%", // Adjust the width for larger desktop screens
+  },
+}));
+
   
   export const ProgressContainer = styled("div")(({ theme }) => ({
     position: "absolute",
